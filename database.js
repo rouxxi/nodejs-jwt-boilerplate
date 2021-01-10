be1129f8c3efd6d9b5dab114031a37750a44521f
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-01-10 18:42:31 +0100

    readme

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const mysql = require("mysql");
+require('dotenv').config();
+const mysql = require('mysql');
 
 const config = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 };
 
 const connection = mysql.createPool(config);
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 require('dotenv').config();
 const connection = require('./database');
 
@@ -17,104 +15,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 1. Register route
-app.post('/register', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res
-      .status(400)
-      .json({ errorMessage: 'Please specify both email and password' });
-  } else {
-    const hash = bcrypt.hashSync(password, 10);
-    connection.query(
-      `INSERT INTO user(email, password) VALUES (?, ?)`,
-      [email, hash],
-      (error, result) => {
-        if (error) {
-          res.status(500).json({ errorMessage: error.message });
-        } else {
-          res.status(201).json({
-            id: result.insertId,
-            email,
-          });
-        }
-      }
-    );
-  }
-});
+// Your code here!
 
-// 2. Login route
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res
-      .status(400)
-      .json({ errorMessage: 'Please specify both email and password' });
-  } else {
-    connection.query(
-      `SELECT * FROM user WHERE email=?`,
-      [email],
-      (error, result) => {
-        if (error) {
-          res.status(500).json({ errorMessage: error.message });
-        } else if (result.length === 0) {
-          res.status(403).json({ errorMessage: 'Invalid email' });
-        } else if (bcrypt.compareSync(password, result[0].password)) {
-          // Passwords match
-          const user = {
-            id: result[0].id,
-            email,
-            password: 'hidden',
-          };
-          const token = jwt.sign({ id: user.id }, JWT_SECRET, {
-            expiresIn: '1 day',
-          });
-          res.status(200).json({ user, token });
-        } else {
-          // Passwords don't match
-          res.status(403).json({ errorMessage: 'Invalid password' });
-        }
-      }
-    );
-  }
-});
-
-// 3. Token middleware
-const authenticateWithJsonWebToken = (req, res, next) => {
-  if (req.headers.authorization !== undefined) {
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, JWT_SECRET, (err) => {
-      if (err) {
-        res
-          .status(401)
-          .json({ errorMessage: "you're not allowed to access these data" });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res
-      .status(401)
-      .json({ errorMessage: "you're not allowed to access these data" });
-  }
-};
-
-// 4. Authenticated route
-app.get('/users', authenticateWithJsonWebToken, (req, res) => {
-  connection.query(`SELECT * FROM user`, (error, result) => {
-    if (error) {
-      res.status(500).json({ errorMessage: error.message });
-    } else {
-      res.status(200).json(
-        result.map((user) => {
-          return { ...user, password: 'hidden' };
-        })
-      );
-    }
-  });
-});
-
-// Don't write anything below this line
+// Don't write anything below this line!
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on port ${SERVER_PORT}.`);
 });
